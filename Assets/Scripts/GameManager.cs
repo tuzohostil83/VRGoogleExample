f47b9c76d4d7851ea9947ab3b95c3bccb6407c7a
Kevin Alonso Acevedo Gonzalez <acevedo19972013@gmail.com>
2020-11-23 23:29:35 -0500

    Primer proyecto funcional

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Toggle toggle = null;
+    public void GuardarOpcion()
+    {
+        if (toggle.isOn)
+        {
+            print("Activado VR");
+        }
+        else
+        {
+            print("Desactivado VR");
+        }
+    }
+    public void CambiarScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+}
---
+++ Assets/Scripts/ManualXRControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class ManualXRControl : MonoBehaviour
+{
+    bool isVR = false;
+    public void StartOrStopVR()
+    {
+        if (!isVR)
+        {
+            StartCoroutine(StartXR());
+            isVR = true;
+        }
+        else
+        {
+            StopXR();
+            isVR = false;
+        }
+    }
+    public IEnumerator StartXR()
+    {
+        Debug.Log("Initializing XR...");
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+        }
+        else
+        {
+            Debug.Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+        }
+    }
+
+    void StopXR()
+    {
+        Debug.Log("Stopping XR...");
+
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Debug.Log("XR stopped completely.");
+    }
+}
